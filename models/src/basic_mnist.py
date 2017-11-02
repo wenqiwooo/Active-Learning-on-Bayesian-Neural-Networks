80bf42cd20732eff804850051fd1010745bdf7d4
therealwenqi <therealwenqi@hotmail.com>
2017-11-03 01:10:27 +0800

    update model

--- models/src/basic_mnist.py
+++ models/src/basic_mnist.py
@@ -276,8 +276,65 @@ class MnistCNN(object):
         sfc_w2, sfc_b2, X))
 
 
+class MnistPlain(object):
+  def __init__(self, mnist, input_dim=784, output_dim=10, iterations=250, 
+      batch_size=100):
+    self.global_step = tf.Variable(
+        initial_value=0, name='global_step', trainable=False)
+
+    self.input_dim = input_dim
+    self.output_dim = output_dim
+    self.iterations = iterations
+    self.batch_size = batch_size
+
+    self.X_placeholder = tf.placeholder(tf.float32, (None, self.input_dim))
+    self.Y_placeholder = tf.placeholder(tf.int32, (None,))
+
+    w_shape = (input_dim, output_dim)
+    w = tf.get_variable('w', w_shape, dtype=tf.float32, 
+        initializer=tf.contrib.layers.xavier_initializer())
+    b = tf.get_variable('b', [w_shape[-1]], dtype=tf.float32,
+        initializer=tf.constant_initializer(0.))
+    self.nn = tf.matmul(self.X_placeholder, w) + b
+
+    self.pred = tf.nn.softmax(self.nn)
+
+    self.loss = tf.reduce_mean(
+        tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=self.nn, labels=self.Y_placeholder))
+
+    self.train = tf.train.AdamOptimizer(
+        learning_rate=1e-3).minimize(self.loss, global_step=self.global_step)
+
+  def optimize(self, sess, mnist):
+    X_batches = []
+    Y_batches = []
+    for i in range(3):
+      X, Y = mnist.train.next_batch(50)
+      X_batches.append(X)
+      Y_batches.append(Y)
+    for i in range(self.iterations):
+      # X_batch, Y_batch = mnist.train.next_batch(self.batch_size)
+      X_batch = X_batches[i % 3]
+      Y_batch = Y_batches[i % 3]
+      sess.run(self.train, feed_dict={
+          self.X_placeholder: X_batch,
+          self.Y_placeholder: Y_batch
+        })
+
+  def validate(self, sess, mnist):
+    X_test = mnist.test.images[:1000]
+    Y_test = mnist.test.labels[:1000]
+    pred = sess.run(self.pred, feed_dict={
+        self.X_placeholder: X_test,
+        self.Y_placeholder: Y_test
+      })
+    pred = np.argmax(pred, axis=1)
+    return (pred == Y_test).mean() * 100
+
+
 class MnistModel(object):
-  def __init__(self, mnist, input_dim=784, output_dim=10, iterations=100, 
+  def __init__(self, mnist, input_dim=784, output_dim=10, iterations=250, 
       batch_size=100):
     self.input_dim = input_dim
     self.output_dim = output_dim
@@ -306,8 +363,16 @@ class MnistModel(object):
         scale={self.pred: mnist.train.num_examples / self.batch_size})
 
   def optimize(self, mnist):
-    for _ in range(self.inference.n_iter):
-      X_batch, Y_batch = mnist.train.next_batch(self.batch_size)
+    X_batches = []
+    Y_batches = []
+    for i in range(3):
+      X, Y = mnist.train.next_batch(50)
+      X_batches.append(X)
+      Y_batches.append(Y)
+    for i in range(self.inference.n_iter):
+      # X_batch, Y_batch = mnist.train.next_batch(self.batch_size)
+      X_batch = X_batches[i % 3]
+      Y_batch = Y_batches[i % 3]
       info_dict = self.inference.update(feed_dict={
           self.X_placeholder: X_batch,
           self.Y_placeholder: Y_batch
@@ -317,16 +382,21 @@ class MnistModel(object):
   def validate(self, mnist, n_samples):
     X_test = mnist.test.images[:1000]
     Y_test = mnist.test.labels[:1000]
-    probs = []
-    for _ in range(n_samples):
+    #probs = []
+    probs = np.zeros((1000, 10))
+    for _ in tqdm(range(n_samples)):
       sw = self.qw.sample()
       sb = self.qb.sample()
       prob = tf.nn.softmax(tf.matmul(X_test, sw) + sb)
-      probs.append(prob.eval())
-    accuracies = []
-    for prob in probs:
-      pred = np.argmax(prob, axis=1)
-      acc = (pred == Y_test).mean() * 100
-      accuracies.append(acc)
+      #probs.append(prob.eval())
+      probs += prob.eval()
+    # accuracies = []
+    # for prob in probs:
+    #   pred = np.argmax(prob, axis=1)
+    #   acc = (pred == Y_test).mean() * 100
+    #   accuracies.append(acc)
+    
+    pred = np.argmax(probs, axis=1)
+    accuracies = (pred == Y_test).mean() * 100
     return accuracies
 
--- models/src/basic_mnist_train.py
+++ models/src/basic_mnist_train.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-from basic_mnist import MnistModel, MnistCNN, MnistMLP
+from basic_mnist import MnistModel, MnistCNN, MnistMLP, MnistPlain
 
 
 flags = tf.app.flags
@@ -11,10 +11,13 @@ flags.DEFINE_string('data_dir', '../data', 'Data directory.')
 def main(_):
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=False)
   model = MnistModel(mnist)
+  #model = MnistPlain(mnist)
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     model.optimize(mnist)
-    acc = model.validate(mnist, 5)
+    acc = model.validate(mnist, 500)
+    # model.optimize(sess, mnist)
+    # acc = model.validate(sess, mnist)
     print(acc)
 
 
