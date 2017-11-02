3ddb0d6e59a1d0329fce10ccec8c50666575e006
therealwenqi <therealwenqi@hotmail.com>
2017-11-02 21:46:24 +0800

    update model

--- models/src/cifar10_bcnn2.py
+++ models/src/cifar10_bcnn2.py
@@ -144,7 +144,7 @@ class Cifar10BCNN(object):
         self.fc_w2: self.qfc_w2, self.fc_b2: self.qfc_b2,
       }, data={self.categorical: self.y})
 
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-4)
+    optimizer = tf.train.AdamOptimizer(learning_rate=1e-4)
 
     iterations = self.epochs * math.ceil(self.data_size / self.batch_size)
     self.inference.initialize(n_iter=iterations, optimizer=optimizer)
