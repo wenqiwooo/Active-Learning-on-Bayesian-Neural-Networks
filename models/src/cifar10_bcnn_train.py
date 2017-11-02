4b7355b2fa3b02653e4645aaaf5ae3d52907fa43
therealwenqi <therealwenqi@hotmail.com>
2017-11-02 12:25:42 +0800

    update model

--- models/src/cifar10_bcnn_train.py
+++ models/src/cifar10_bcnn_train.py
@@ -96,7 +96,7 @@ def main(_):
   Y_test = np.squeeze(Y_test)
 
   with tf.Session() as sess:
-    model = Cifar10BCNN(FLAGS.epochs, len(images), FLAGS.batch_size)
+    model = Cifar10BCNN(FLAGS.epochs, len(X_train), FLAGS.batch_size)
     sess.run(tf.global_variables_initializer())
     model.optimize(X_train, Y_train, FLAGS.epochs, FLAGS.batch_size)
     acc = model.validate(X_test, Y_test, FLAGS.batch_size, 3)
