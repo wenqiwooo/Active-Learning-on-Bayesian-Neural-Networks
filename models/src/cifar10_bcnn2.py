509df379bf58392ea980e483c804da9be3877fda
therealwenqi <therealwenqi@hotmail.com>
2017-11-10 21:46:00 +0800

    update model

--- models/src/cifar10_bcnn2.py
+++ models/src/cifar10_bcnn2.py
@@ -114,7 +114,7 @@ class BayesianDropout(object):
       }, data={self.categorical: self.y})
 
     self.lr = tf.train.exponential_decay(
-        1e-5, self.global_step, 50000, 0.96, staircase=True)
+        1e-6, self.global_step, 50000, 0.96, staircase=True)
 
     self.optimizer = tf.train.AdamOptimizer(self.lr)
 
